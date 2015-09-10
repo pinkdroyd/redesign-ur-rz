5e09fb56ff42efaa4dac7cc4b74fa12b945de5c0
pinkdroyd <ben@inrane.de>
2015-09-10 10:10:18 +0200

    added mind map for new structure

---
+++ Card Sorting/Ergebnisse/neue struktur.mm
@@ -0,0 +1,123 @@
+<map version="freeplane 1.3.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="0" ID="ID_1723255651" CREATED="1283093380553" MODIFIED="1441870314283"><hook NAME="MapStyle">
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node">
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right">
+<stylenode LOCALIZED_TEXT="default" MAX_WIDTH="600" COLOR="#000000" STYLE="as_parent">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.note"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="3"/>
+<node TEXT="Mein RZ-Account" POSITION="right" ID="ID_669248540" CREATED="1441869848084" MODIFIED="1441869857971">
+<edge COLOR="#ff0000"/>
+<node TEXT="RZ-Passwort &#xe4;ndern" ID="ID_751792019" CREATED="1441869971630" MODIFIED="1441869979857"/>
+<node TEXT="RZ-Konto" ID="ID_1940107942" CREATED="1441869983337" MODIFIED="1441869985622"/>
+<node TEXT="RZ-Account mit Passwort verwenden" ID="ID_1041095115" CREATED="1441869988999" MODIFIED="1441869995571"/>
+<node TEXT="Pers&#xf6;nliche Homepage" ID="ID_468570011" CREATED="1441870000966" MODIFIED="1441870004109"/>
+<node TEXT="RZ-Passwort vergessen" ID="ID_576465163" CREATED="1441870019242" MODIFIED="1441870031311"/>
+</node>
+<node TEXT="Drucken" POSITION="right" ID="ID_1629054648" CREATED="1441869921196" MODIFIED="1441869924834">
+<edge COLOR="#00ff00"/>
+<node TEXT="Verf&#xfc;gbare Drucker" ID="ID_1857720403" CREATED="1441870042651" MODIFIED="1441870047942"/>
+<node TEXT="WebPrint" ID="ID_1778042055" CREATED="1441870054716" MODIFIED="1441870058522"/>
+</node>
+<node TEXT="E-Mail" POSITION="right" ID="ID_1765525429" CREATED="1441869925215" MODIFIED="1441869928027">
+<edge COLOR="#ff00ff"/>
+<node TEXT="Zugang zum E-Mail-Account" ID="ID_1262943197" CREATED="1441870096491" MODIFIED="1441870102907"/>
+<node TEXT="E-Mail versenden und empfangen" ID="ID_153571769" CREATED="1441870161782" MODIFIED="1441870166332"/>
+</node>
+<node TEXT="CIP-Pools" POSITION="right" ID="ID_1144884299" CREATED="1441869929468" MODIFIED="1441869931610">
+<edge COLOR="#00ffff"/>
+<node TEXT="Schadensmeldungen von Rechnern und Ger&#xe4;ten in CIP-Pools" ID="ID_1359084573" CREATED="1441872090408" MODIFIED="1441872091858"/>
+<node TEXT="CIP-Pool-Reservierung" ID="ID_1159743113" CREATED="1441872094739" MODIFIED="1441872101111"/>
+</node>
+<node TEXT="Software" POSITION="right" ID="ID_1890092100" CREATED="1441869931909" MODIFIED="1441869934117">
+<edge COLOR="#ffff00"/>
+<node TEXT="Content Management System Imperia" ID="ID_496886877" CREATED="1441870124410" MODIFIED="1441870131584"/>
+<node TEXT="Softwarebeschaffung &#xfc;ber die Universit&#xe4;t Regensburg" ID="ID_965152581" CREATED="1441870448315" MODIFIED="1441870450110"/>
+<node TEXT="Windows f&#xfc;r Mitarbeiter und Studierende" ID="ID_653088175" CREATED="1441871781432" MODIFIED="1441871793984"/>
+<node TEXT="Software f&#xfc;r Windows" ID="ID_1584644035" CREATED="1441871796445" MODIFIED="1441871799153"/>
+<node TEXT="Linux" ID="ID_62480224" CREATED="1441871800775" MODIFIED="1441871802542"/>
+<node TEXT="MS Office" ID="ID_1466416717" CREATED="1441871804236" MODIFIED="1441871807072"/>
+<node TEXT="Softwareangebot f&#xfc;r Studierende" ID="ID_1625603321" CREATED="1441871809371" MODIFIED="1441871827016"/>
+<node TEXT="Softwarekatalog" ID="ID_667733682" CREATED="1441871831716" MODIFIED="1441871834550"/>
+<node TEXT="Softwaredokumentation" ID="ID_1416097091" CREATED="1441871837441" MODIFIED="1441871840726"/>
+<node TEXT="Empfohlene Apps der Uni Regensburg" ID="ID_1154819793" CREATED="1441871842737" MODIFIED="1441871850575"/>
+</node>
+<node TEXT="FAQ" POSITION="right" ID="ID_703933153" CREATED="1441869934333" MODIFIED="1441869935660">
+<edge COLOR="#7c0000"/>
+</node>
+<node TEXT="Dienste" POSITION="right" ID="ID_72266525" CREATED="1441869938096" MODIFIED="1441869939341">
+<edge COLOR="#00007c"/>
+<node TEXT="Infostand" ID="ID_1779496599" CREATED="1441870175017" MODIFIED="1441870177550"/>
+<node TEXT="Erweiterter IT-Support" ID="ID_916056347" CREATED="1441870205073" MODIFIED="1441870209872"/>
+<node TEXT="Netzlaufwerke und Dateidienste (z.B. Netstorage)" ID="ID_474802615" CREATED="1441870219416" MODIFIED="1441870223154"/>
+<node TEXT="VPN" ID="ID_943488088" CREATED="1441870234732" MODIFIED="1441870236545"/>
+</node>
+<node TEXT="WLAN und Internetzugang" POSITION="right" ID="ID_770433897" CREATED="1441869940664" MODIFIED="1441869946521">
+<edge COLOR="#007c00"/>
+<node TEXT="Internet im Wohnheim" ID="ID_1379046742" CREATED="1441870192615" MODIFIED="1441870198395"/>
+<node TEXT="WLAN-Nutzung der Universit&#xe4;t Regensburg" ID="ID_1245377829" CREATED="1441870260757" MODIFIED="1441870262109"/>
+</node>
+<node TEXT="Hardware" POSITION="right" ID="ID_189916393" CREATED="1441871853709" MODIFIED="1441871859718">
+<edge COLOR="#0000ff"/>
+<node TEXT="Rabattaktionen beim Kauf von Apple-Produkten" ID="ID_1914333919" CREATED="1441871982940" MODIFIED="1441872036333"/>
+<node TEXT="Einkauf von Hardware" ID="ID_1460123355" CREATED="1441872042741" MODIFIED="1441872045571"/>
+<node TEXT="Lieferservice von Hardware" ID="ID_409237758" CREATED="1441872049124" MODIFIED="1441872056104"/>
+<node TEXT="Reperaturservice f&#xfc;r Hardware" ID="ID_940397624" CREATED="1441872059130" MODIFIED="1441872071491"/>
+<node TEXT="Ger&#xe4;teausleihe" ID="ID_308391072" CREATED="1441872077121" MODIFIED="1441872080312"/>
+</node>
+<node TEXT="Verwaltung (?)" POSITION="right" ID="ID_542155170" CREATED="1441870395125" MODIFIED="1441870430627">
+<edge COLOR="#ff0000"/>
+<node TEXT="Workgroupmanager" ID="ID_1250960717" CREATED="1441870403295" MODIFIED="1441870406940"/>
+<node TEXT="Formulare" ID="ID_1038458082" CREATED="1441869953276" MODIFIED="1441872299650"/>
+</node>
+<node TEXT="Vorlesungen und Pr&#xfc;fungen" POSITION="right" ID="ID_1085113772" CREATED="1441872426119" MODIFIED="1441872430658">
+<edge COLOR="#00ff00"/>
+<node TEXT="Computergest&#xfc;tzte Pr&#xfc;fungen" ID="ID_1694458829" CREATED="1441872432662" MODIFIED="1441872441069"/>
+<node TEXT="GRIPS" ID="ID_1590563630" CREATED="1441872443409" MODIFIED="1441872444784"/>
+</node>
+</node>
+</map>
